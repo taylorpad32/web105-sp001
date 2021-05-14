3ddb5d77e1c442b8fcdfafedafaf2c6a998514b7
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-05-14 08:49:35 -0500

    Add files via upload

---
+++ grid-practice/myscript.js
@@ -0,0 +1,15 @@
+function change01() {
+    document.getElementById("large").src = "images/edinburgh-ps.jpg"; 
+}
+function change02() {
+    document.getElementById("large").src = "images/lake-ps.jpg";
+}
+function change03() {
+    document.getElementById("large").src = "images/loneliness-ps.jpg";
+}
+function change04() {
+    document.getElementById("large").src = "images/rome-ps.jpg";
+}
+function change05() {
+    document.getElementById("large").src = "images/edinburgh-ps.jpg";
+}
